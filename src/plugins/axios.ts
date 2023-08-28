1324fb83f021824cc1f4d4db1096cbc3774d01b1
damouu <mouadsehbaoui@gmail.com>
2023-08-28 17:32:29 +0900

    creates a pluging so that the axios object can be instantiated outside of the main.js file

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,9 +1,12 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://pokeapi.co/api/v2',
+    baseURL: 'http://localhost:8080/api',
     headers: {
         'Content-type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:8080/',
+        'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+        'Access-Control-Allow-Credentials': true
     },
 })
 
