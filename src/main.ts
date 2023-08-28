e3a67cc5e504975270d72a9928a1576c1ca2c683
damouu <mouadsehbaoui@gmail.com>
2023-08-28 16:10:59 +0900

    receives the axios objects and injects it into the Vueapp so that the axios object can be inserted into any component

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
 import './assets/main.css'
-
 import {createApp} from 'vue'
 import {createPinia} from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import instance from "@/plugins/axios";
 
-const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
-
+const app = createApp(App).use(router, createPinia());
+app.provide('axios', instance);
 app.mount('#app')
