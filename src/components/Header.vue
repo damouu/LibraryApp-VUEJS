6fec5aecf03ad3569c24770a4f9ab5b2cb50e8ce
damouu <mouadsehbaoui@gmail.com>
2023-09-13 11:11:32 +0900

    #POLARISDPVUEJS-5 Done Unassigned Fixed Done
    Feature Header.vue Component
    
    creates the Header.vue component reusable in the whole project.

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light position-fixed fixed-top border border-danger">
+    <ButtonCheck/>
+    <a class="navbar-brand">VueApp</a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <RouterLink to="/" class="nav-link active" aria-current="page">ホームページ</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/about" class="nav-link active" aria-current="page">お知らせ</RouterLink>
+        </li>
+        <li class="nav-item dropdown">
+          <RouterLink to="/student" class="nav-link active" aria-current="page">学生のページ</RouterLink>
+        </li>
+      </ul>
+      <FormSearch/>
+    </div>
+  </nav>
+</template>
+<script setup lang="ts">
+import ButtonCheck from "@/components/ButtonCheck.vue";
+import FormSearch from "@/components/FormSearch.vue";
+</script>
