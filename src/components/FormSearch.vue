90121d0959dbffd4707e2a428b1180f6e75287c8
damouu <mouadsehbaoui@gmail.com>
2023-09-13 11:10:52 +0900

    #POLARISDPVUEJS-5 Done Unassigned Fixed Done
    Feature FormSearch.vue Component
    
    creates a FormSearch.vue component reusable in the whole project.

---
+++ src/components/FormSearch.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="pe-2">
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-success rounded border border-secondary ms-2 me-2">
+        <i class="bi bi-person-fill"></i>
+      </button>
+    </form>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
