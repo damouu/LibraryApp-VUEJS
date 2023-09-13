47f73698f0b43e0425bfb18dc097b7747786cc99
damouu <mouadsehbaoui@gmail.com>
2023-09-13 11:14:32 +0900

    #POLARISDPVUEJS-5 Done Unassigned Fixed Done
    Component PaginationTable.vue
    
    creates the PaginationTable.vue component reusable in the whole project.

---
+++ src/components/PaginationTable.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="row">
+    <div class="col-3 offset-3">
+      <p class="text-primary-emphasis">aside menu</p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
