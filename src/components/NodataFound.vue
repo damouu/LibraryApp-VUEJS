a582644d340d64187ff8205b817c1b96da4261ad
damouu <mouadsehbaoui@gmail.com>
2023-09-13 11:14:13 +0900

    #POLARISDPVUEJS-5 Done Unassigned Fixed Done
    Component NodataFound.vue
    
    creates the NodataFound.vue component reusable in the whole project.

---
+++ src/components/NodataFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="greetings row">
+    <div class="col-3">
+      <!--    <h1 class="green">{{ dede }}</h1>-->
+      <h3>
+        No data was found, maybe the API is down???
+        <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+        <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      </h3>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// defineEmits()
+// defineProps<{
+//   dede: string
+// }>()
+</script>
