90050bbcd4f5b247c0deaf4e541e4c5f98793df5
damouu <mouadsehbaoui@gmail.com>
2023-09-15 16:59:26 +0900

    #POLARISDPVUEJS-6 Done Unassigned Fixed DOne
    Refactor: Student.ts
    
    refactors the Student.ts to a real class with attributes, constructor and getter/ setters rather than the previous type/ interface structure.

--- src/types/Student.ts
+++ src/types/Student.ts
@@ -1,6 +1,45 @@
-interface Student {
-    uuid: number,
-    name: string,
-    email: string,
-    dob: number,
+export class Student {
+    private _uuid: string;
+    private _name: string;
+    private _email: string;
+    private _dob: string;
+
+    constructor(uuid: string, name: string, email: string, dob: string) {
+        this._uuid = uuid;
+        this._name = name;
+        this._email = email;
+        this._dob = dob;
+    }
+
+    get uuid(): string {
+        return this._uuid;
+    }
+
+    set uuid(value: string) {
+        this._uuid = value;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
+    }
+
+    get email(): string {
+        return this._email;
+    }
+
+    set email(value: string) {
+        this._email = value;
+    }
+
+    get dob(): string {
+        return this._dob;
+    }
+
+    set dob(value: string) {
+        this._dob = value;
+    }
 }
