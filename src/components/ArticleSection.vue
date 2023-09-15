63c108b0d4cc726f47aab7950cc189204edc7d8b
damouu <mouadsehbaoui@gmail.com>
2023-09-15 17:07:19 +0900

    #POLARISDPVUEJS-6 Done Unassigned Fixed DOne
    Creates: ArticleSection.vue
    
    creates a new component to be used as a page's section content.

---
+++ src/components/ArticleSection.vue
@@ -0,0 +1,7 @@
+<template>
+  <article>
+    <section>
+      <p class="text-warning">test section</p>
+    </section>
+  </article>
+</template>
--- src/components/NodataFound.vue
+++ src/components/NodataFound.vue
@@ -1,19 +1,15 @@
 <template>
-  <div class="greetings row">
-    <div class="col-3">
-      <!--    <h1 class="green">{{ dede }}</h1>-->
-      <h3>
-        No data was found, maybe the API is down???
-        <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-        <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-      </h3>
-    </div>
-  </div>
+  <h1 class="green">{{ props.dede }}</h1>
+  <h3>
+    No data was found, maybe the API is down???
+    {{ props.dede}}
+    <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+  </h3>
 </template>
 
 <script setup lang="ts">
-// defineEmits()
-// defineProps<{
-//   dede: string
-// }>()
+const props = defineProps({
+  dede: {type: String, required: true},
+})
 </script>
