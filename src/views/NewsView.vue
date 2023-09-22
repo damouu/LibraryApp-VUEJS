34909c99d7f64c77a090da7151c97a5417f625ed
damouu <mouadsehbaoui@gmail.com>
2023-09-22 12:38:08 +0900

    #POLARISDPVUEJS-11 Done Unassigned Fixed Done
    Create: NewsView.vue component
    
    creates NewsView.vue component

---
+++ src/views/NewsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="row mb-5">
+    <div>
+      <TitleMessage title="フォトギャラリー"/>
+    </div>
+  </section>
+  <section class="row">
+    <main>
+      <CardGrid/>
+    </main>
+  </section>
+</template>
+
+
+<script setup lang="ts">
+import CardGrid from "@/components/CardGrid.vue";
+import TitleMessage from "@/components/TitleMessage.vue";
+</script>
