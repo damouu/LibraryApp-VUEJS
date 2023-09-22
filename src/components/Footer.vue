b7e71187a5a44c58b75577eced5e58494c12315d
damouu <mouadsehbaoui@gmail.com>
2023-09-22 16:03:54 +0900

    #POLARISDPVUEJS-14 Done Unassigned Fixed Done
    Style: top margin
    
    adds top margin to the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center p-5 border border-primary p-2 bg-success w-100">
+  <div class="text-center p-5 border border-primary p-2 bg-success w-100 mt-5">
     <div class="row">
       <div class="col">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
