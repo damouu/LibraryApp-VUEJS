9450388b00e0486dd025afc3e8b4f56ab2ff998e
damouu <mouadsehbaoui@gmail.com>
2023-09-22 12:17:46 +0900

    #POLARISDPVUEJS-11 Done Unassigned Fixed Done
    Refactor: Changes
    
    removes the form attribute to a simpler button attribute.

--- src/components/FormSearch.vue
+++ src/components/FormSearch.vue
@@ -1,12 +1,5 @@
 <template>
-  <div class="pe-2">
-    <form class="d-flex">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-success rounded border border-secondary ms-2 me-2">
-        <i class="bi bi-person-fill"></i>
-      </button>
-    </form>
-  </div>
+  <button class="btn rounded ms-2 me-2">
+    <i class="bi bi-list"></i>
+  </button>
 </template>
-<script setup lang="ts">
-</script>
