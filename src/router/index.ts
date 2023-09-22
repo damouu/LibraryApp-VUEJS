2fcecad3232e9be362a40b44aa8c913d34de44fb
damouu <mouadsehbaoui@gmail.com>
2023-09-22 12:36:52 +0900

    #POLARISDPVUEJS-11 Done Unassigned Fixed Done
    Refactor: renames path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DataView from "@/components/DataView.vue";
+import NewsView from "@/views/NewsView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,9 +12,9 @@ const router = createRouter({
             component: HomeView
         },
         {
-            path: '/about',
-            name: 'about',
-            component: () => import('../views/AboutView.vue')
+            path: '/news',
+            name: 'news',
+            component: NewsView,
         },
         {
             path: '/student',
