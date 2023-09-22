4cfd030821982d2ba665eaa62f25ad7f172b521c
damouu <mouadsehbaoui@gmail.com>
2023-09-22 15:59:50 +0900

    #POLARISDPVUEJS-14 Done Unassigned Fixed Done
    Style: centers + Icons
    
    adds icons and labels to the Header and places those icons into the center of ther header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,36 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-success position-fixed fixed-top border border-primary">
-    <ButtonCheck/>
-    <a class="navbar-brand">VueApp</a>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <RouterLink to="/" class="nav-link active" aria-current="page">ホームページ</RouterLink>
-        </li>
-        <li class="nav-item">
-          <RouterLink to="/news" class="nav-link active" aria-current="page">ニュース</RouterLink>
-        </li>
-        <li class="nav-item dropdown">
-          <RouterLink to="/student" class="nav-link active" aria-current="page">学生のページ</RouterLink>
-        </li>
-      </ul>
-      <FormSearch/>
-    </div>
-  </nav>
+  <div class="row">
+    <nav class="navbar navbar-expand-lg navbar-light bg-success position-fixed fixed-top border border-primary">
+      <ButtonCheck/>
+      <div class="collapse navbar-collapse offset-4 gx-3" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center mt-3">
+          <li class="nav-item me-3">
+            <i class="bi bi-book-fill h4"></i>
+            <RouterLink to="/" class="nav-link active" aria-current="page">ブック</RouterLink>
+          </li>
+          <li class="nav-item me-2">
+            <i class="bi bi-newspaper h4"></i>
+            <RouterLink to="/news" class="nav-link active" aria-current="page">ニュース</RouterLink>
+          </li>
+          <li class="nav-item dropdown me-4 ms-1">
+            <i class="bi bi-backpack2-fill h4"></i>
+            <RouterLink to="/student" class="nav-link active" aria-current="page">学生</RouterLink>
+          </li>
+          <li class="nav-item dropdown">
+            <i class="bi bi-journal-bookmark-fill h4"></i>
+            <RouterLink to="/student" class="nav-link active" aria-current="page">授業</RouterLink>
+          </li>
+          <li class="nav-item dropdown ms-4">
+            <i class="bi bi-mortarboard-fill h4"></i>
+            <RouterLink to="/student" class="nav-link active" aria-current="page">教師</RouterLink>
+          </li>
+        </ul>
+        <div>
+          <FormSearch/>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script setup lang="ts">
