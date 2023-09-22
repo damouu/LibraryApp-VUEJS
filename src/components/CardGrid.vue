acf0a9b4b7d72cb4d4aa628ab0ef2a84f3a9e6ce
damouu <mouadsehbaoui@gmail.com>
2023-09-22 12:53:01 +0900

    #POLARISDPVUEJS-11 Done Unassigned Fixed Done
    Creates: CardGrid.vue
    
    creates a component to print in a card Grid format, all the latest news.

---
+++ src/components/CardGrid.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="row row-cols-1 row-cols-md-3 g-4 mb-5">
+    <div class="col">
+      <div class="card">
+        <img src="../assets/pokemon-pokemon-red-and-blue-wallpaper-preview.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-primary">La pose de star 😎</h5>
+          <p class="card-text">Mothôlde qui tape une petite pose telle une star de cinéma au festival de Cannes alors
+            qu'elle a
+            le charisme d'un phacochère + elle va s'endormir apres </p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="../assets/pokemon-pokemon-red-and-blue-wallpaper-preview.jpg" class="card-img-top"
+             alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-primary">Le cambriolage <i class="bi bi-incognito"></i></h5>
+          <p class="card-text">Mothôlde qui s'apprête à entrer par effraction chez des Japonais qui l'ont invité sans
+            frapper à la porte avant d'entrer et et les mains vident en plus. 🤣</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="../assets/pokemon-pokemon-red-and-blue-wallpaper-preview.jpg"
+             class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-primary">La Voleuse 🍟</h5>
+          <p class="card-text">Quand il s'agit de bouffe, Mothôlde n'est pas en reste. ces délicieux macarons qui m'ont
+            etais offert par mon バイト dévorer devant mes yeux par elle 😥</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="../assets/pokemon-pokemon-red-and-blue-wallpaper-preview.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-primary">L'exhibitionniste 🍑</h5>
+          <p class="card-text">Mothôlde, quoiqu'un peu réserver de nature, elle adore se faire tripoter certaines
+            parties du corps,
+            et sa, même en public a la vue de tous.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="../assets/pokemon-pokemon-red-and-blue-wallpaper-preview.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-primary">Le mauvais choix 🖕 </h5>
+          <p class="card-text">Bon, là je suis un peu en manque d'idées haha, donc disons qu'on va faire un peu de
+            remplissage pour cette vignette-ci. J'sais que tu ne lis pas les texts </p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="../assets/pokemon-pokemon-red-and-blue-wallpaper-preview.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-primary">l'instagrameuse <i class="bi bi-instagram"></i></h5>
+          <p class="card-text">Mothôlde l'instagrameuse, elle prend en photo tous
+            ses repas pour les poster sur son compte d'instagrameuse afin de récolter des likes ❤ et de vues 👁</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="../assets/535318.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-warning">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+            content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="../assets/535318.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-warning">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+            content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="../assets/535318.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-warning">Card title</h5>
+          <p class="card-text">This is a longer cards with supporting text below as a natural lead-in to additional
+            content.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
