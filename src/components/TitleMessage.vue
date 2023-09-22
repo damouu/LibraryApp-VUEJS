0680032eb1ea806fa10ab87fa08b44602093aa83
damouu <mouadsehbaoui@gmail.com>
2023-09-22 11:54:32 +0900

    #POLARISDPVUEJS-11 Done Unassigned Fixed Done
    Refactor: TitleMessage.vue
    
    independents the title section by receiving a title text as a prop from any parent component.

---
+++ src/components/TitleMessage.vue
@@ -0,0 +1,10 @@
+<template>
+  <H2 class="text-warning text-center">{{ props.title }}</H2>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  title: {type: String, required: true},
+});
+</script>
