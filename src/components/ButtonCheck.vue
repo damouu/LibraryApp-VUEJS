792fb1efaaf6a8d400081e25de58ba62118d565a
damouu <mouadsehbaoui@gmail.com>
2023-10-03 17:16:17 +0900

    #POLARISDPVUEJS-19 Done Unassigned Fixed Done
    Refactor: router-link
    
    places the router-link inside the button component

--- src/components/ButtonCheck.vue
+++ src/components/ButtonCheck.vue
@@ -1,5 +1,7 @@
 <template>
-  <button class="btn rounded ms-2 me-2">
-    <i class="bi bi-chat-quote h4">VueApp</i>
-  </button>
+  <router-link v-slot="{ navigate }" custom to="/">
+    <button class="btn rounded ms-2 me-2" role="link" @click="navigate">
+      <i class="bi bi-chat-quote h4">VueApp</i>
+    </button>
+  </router-link>
 </template>
