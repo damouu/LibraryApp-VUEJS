e81bd787de6cb0a4d483d25d060f459ca5a14652
damouu <mouadsehbaoui@gmail.com>
2023-11-03 11:02:40 +0900

    #POLARISDPVUEJS-18 Done Unassigned Fixed Done
    Refactor: searchBook function
    
    creates the function searchBook to search for a book by the given UUID

--- src/components/CardGrid.vue
+++ src/components/CardGrid.vue
@@ -1,12 +1,39 @@
 <template>
-  <div class="row row-cols-1 row-cols-md-5 g-4 mb-5" ref="scrollComponent">
-    <div v-for="book in bookStore.bookList" :key="book.uuid">
+  <div class="d-flex mb-3">
+    <div class="d-inline-block">
+      <p>Number of books found: &nbsp; </p>
+    </div>
+    <div class="d-inline-block">
+      <h5 class="text-primary"> {{ booksCount }} </h5>
+    </div>
+    <div>
+      <span v-if="booksCount.value == 0" class="text-danger">No result found with title: {{ props.input }}</span>
+    </div>
+  </div>
+  <div v-if="!bookStore.book.uuid || !props.input">
+    <div ref="scrollComponent" class="row row-cols-1 row-cols-md-5 g-4 mb-5">
+      <div v-for="book in bookStore.bookList" :key="book.uuid">
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+            <img alt="..." class="card-img-top" src="../assets/pokemon-pokemon-red-and-blue-wallpaper-preview.jpg">
+            <div class="card-body">
+              <h5 class="card-title text-primary"> {{ book.title }} 📕</h5>
+              <p class="card-text">{{ book.genre }}, {{ book.publisher }}, {{ book.totalPages }}, </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-else>
+    <div ref="scrollComponent" class="row row-cols-1 row-cols-md-5 g-4 mb-5">
       <div class="col">
         <div class="card" style="width: 18rem;">
-          <img src="../assets/pokemon-pokemon-red-and-blue-wallpaper-preview.jpg" class="card-img-top" alt="...">
+          <img alt="..." class="card-img-top" src="../assets/pokemon-pokemon-red-and-blue-wallpaper-preview.jpg">
           <div class="card-body">
-            <h5 class="card-title text-primary">Title: {{ book.title }} 📕</h5>
-            <p class="card-text">{{ book.genre }}, {{ book.publisher }}, {{ book.totalPages }}, </p>
+            <h5 class="card-title text-primary"> {{ bookStore.book.title }} 📕</h5>
+            <p class="card-text">{{ bookStore.book.genre }}, {{ bookStore.book.publisher }},
+              {{ bookStore.book.totalPages }}, </p>
           </div>
         </div>
       </div>
@@ -15,13 +42,43 @@
 </template>
 
 <script setup lang="ts">
-import {onMounted, onUnmounted, ref} from "vue";
+import {computed, onMounted, onUnmounted, ref, watch} from "vue";
 import {useBookStore} from "@/stores/Book";
+import {data, errors} from "msw";
 
 const bookStore = useBookStore();
 const scrollComponent = ref(null);
+const props = defineProps({input: String,})
+const wordUpperCase = ref<string | null>(null);
+bookStore.getBooks(1, 20);
+const bookListCopy = computed(() => [...bookStore.bookList])
+const booksCount = computed(() => {
+  return bookStore.bookList.length
+})
 
-bookStore.getBooks();
+function searchBook() {
+  if (props.input[8] == '-' && props.input[13] == '-' && props.input[18] == '-' && props.input[23] == '-') {
+    const promise = bookStore.getBookUUID(props.input)
+    promise.then(function (value) {
+    }, function (reason) {
+    });
+  } else if (props.input?.length > 0) {
+    let words = props.input?.split(" ")
+    words.forEach((value, index, array) => {
+      if (array[value] != typeof undefined) {
+        array[index] = value.charAt(0).toUpperCase() + value.substring(1)
+      }
+    })
+    words = words.join(' ')
+    wordUpperCase.value = words
+  } else {
+    wordUpperCase.value = null
+  }
+}
+
+watch(() => props.input, () => {
+  setTimeout(searchBook, 1000)
+});
 
 onMounted(() => {
   window.addEventListener("scroll", handleScroll)
@@ -33,9 +90,8 @@ onUnmounted(() => {
 
 function handleScroll(): void {
   let element = scrollComponent.value
-  if (element.getBoundingClientRect().bottom < window.innerHeight) {
+  if (element.getBoundingClientRect().bottom < window.innerHeight && bookStore.bookList.length >= 20) {
     bookStore.fetchMoreBooks();
   }
 }
-
 </script>
