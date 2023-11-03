526bc4dc2a5f620041e36982fef8a8c8a4c6fdf1
damouu <mouadsehbaoui@gmail.com>
2023-11-03 10:58:49 +0900

    #POLARISDPVUEJS-18 Done Unassigned Fixed Done
    Refactor: implements emits
    
    implements the two component and implements into the view a way to pass the typed input from the SearchBar.vue component to the CardGrid.vue component via emits.

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,12 +1,15 @@
 <template>
   <section class="row">
-    <div>
-      <TitleMessage title="フォトギャラリー"/>
+    <div class="offset-4 col-4 mb-5 mt-5">
+      <TitleMessage title="Books gallery"/>
+      <div>
+        <Search @inputEmit="pageEmitReceived"/>
+      </div>
     </div>
   </section>
   <section class="row">
     <main>
-      <CardGrid/>
+      <CardGrid :input="inputReceived"/>
     </main>
   </section>
 </template>
@@ -15,4 +18,13 @@
 <script setup lang="ts">
 import CardGrid from "@/components/CardGrid.vue";
 import TitleMessage from "@/components/TitleMessage.vue";
+import Search from "@/components/SearchBar.vue";
+import {ref} from "vue";
+
+let inputReceived: String = ref(null);
+
+function pageEmitReceived(inputEmit): void {
+  inputReceived.value = inputEmit;
+}
+
 </script>
