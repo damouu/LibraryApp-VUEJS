17de5e967557f00da06a05fb32764f03565508c0
damouu <mouadsehbaoui@gmail.com>
2023-11-03 11:01:20 +0900

    #POLARISDPVUEJS-18 Done Unassigned Fixed Done
    Creates: creates SearchBar.vue component
    
    creates a new searchBar component that will be used to allow users to search  resources.

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="input-group ps-1">
+    <i class="input-group-text bi bi-search "></i>
+    <input id="search-input" v-model="input" aria-describedby="basic-addon1" aria-label="Username" class="form-control"
+           placeholder="Search"
+           type="text" @input="toBeCall()">
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from "vue";
+
+let input = ref();
+
+const emit = defineEmits<{ (e: 'inputEmit', userInput: String) }>()
+
+function toBeCall() {
+  emit('inputEmit', input.value);
+}
+
+</script>
