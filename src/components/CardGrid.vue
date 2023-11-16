d03cd0ca66465650b4f4bed0152c59b9be4f4113
damouu <mouadsehbaoui@gmail.com>
2023-11-16 12:06:38 +0900

    #POLARISDPVUEJS-29 Done Unassigned Fixed Done
    Refactor: Books Grid refactor
    
    implements a debounce function rather than using a watch function to search everytime a user has finished to input something in the search field.

--- src/components/CardGrid.vue
+++ src/components/CardGrid.vue
@@ -1,4 +1,5 @@
 <template>
+  <input v-model="searchText" type="text" @input="onInput"/>
   <div class="d-flex mb-3">
     <div class="d-inline-block">
       <p>Number of books found: &nbsp; </p>
@@ -11,9 +12,24 @@
     </div>
   </div>
   <div ref="carousel">
-    <div v-if="!bookStore.book.uuid || !props.input">
+    <div v-if="bookStore.bookListTitle.length == 0  && bookStore.book.uuid == null">
       <div ref="scrollComponent" class="row row-cols-1 row-cols-md-5 g-4 mb-5">
         <div v-for="book in bookStore.bookList" :key="book.uuid">
+          <div class="col">
+            <div class="card" style="width: 18rem;">
+              <img alt="..." class="card-img-top" src="../assets/pokemon-pokemon-red-and-blue-wallpaper-preview.jpg">
+              <div class="card-body">
+                <h5 class="card-title text-primary"> {{ book.title }} 📕</h5>
+                <p class="card-text">{{ book.genre }}, {{ book.publisher }}, {{ book.totalPages }}, </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else-if="bookStore.bookListTitle.length > 0">
+      <div ref="scrollComponent" class="row row-cols-1 row-cols-md-5 g-4 mb-5">
+        <div v-for="book in bookStore.bookListTitle" :key="book.uuid">
           <div class="col">
             <div class="card" style="width: 18rem;">
               <img alt="..." class="card-img-top" src="../assets/pokemon-pokemon-red-and-blue-wallpaper-preview.jpg">
@@ -44,9 +60,9 @@
 </template>
 
 <script setup lang="ts">
-import {computed, onMounted, onUnmounted, onUpdated, ref, watch} from "vue";
+import {onMounted, onUnmounted, onUpdated, ref, watch} from "vue";
 import {useBookStore} from "@/stores/Book";
-import {data, errors} from "msw";
+import debounce from "lodash/debounce"
 
 const bookStore = useBookStore();
 const scrollComponent = ref(null);
@@ -55,13 +71,28 @@ const booksCount = ref<number>(null)
 const props = defineProps({input: String,})
 const wordUpperCase = ref<string | null>(null);
 bookStore.getBooks(1, 20);
-const bookListCopy = computed(() => [...bookStore.bookList])
+const searchText = ref<String>("");
 
-function searchBook() {
-  if (props.input[8] == '-' && props.input[13] == '-' && props.input[18] == '-' && props.input[23] == '-') {
-    bookStore.getBookUUID(props.input)
-  } else if (props.input?.length > 0) {
-    let words = props.input?.split(" ")
+const onInput = debounce(() => {
+  if (searchText.value) {
+    searchBook(searchText.value)
+  }
+}, 1000)
+
+watch(searchText, (newValue) => {
+  if (newValue == 0) {
+    bookStore.bookListTitle.length = 0
+    for (let x in bookStore.book) {
+      bookStore.book[x] = undefined
+    }
+  }
+})
+
+function searchBook(searchText) {
+  if (searchText[8] == '-' && searchText[13] == '-' && searchText[18] == '-' && searchText[23] == '-') {
+    bookStore.getBookUUID(searchText)
+  } else if (searchText?.length > 0) {
+    let words = searchText?.split(" ")
     words.forEach((value, index, array) => {
       if (array[value] != typeof undefined) {
         array[index] = value.charAt(0).toUpperCase() + value.substring(1)
@@ -69,14 +100,18 @@ function searchBook() {
     })
     words = words.join(' ')
     wordUpperCase.value = words
+    bookStore.getBookTitle(wordUpperCase.value)
   } else {
     wordUpperCase.value = null
   }
 }
 
-watch(() => props.input, () => {
-  setTimeout(searchBook, 1000)
-});
+function handleScroll(): void {
+  let element = scrollComponent.value
+  if (element.getBoundingClientRect().bottom < window.innerHeight && booksCount.value >= 20) {
+    bookStore.fetchMoreBooks();
+  }
+}
 
 onMounted(() => {
   window.addEventListener("scroll", handleScroll)
@@ -100,10 +135,4 @@ watch(() => props.input,
     },
     {deep: true})
 
-function handleScroll(): void {
-  let element = scrollComponent.value
-  if (element.getBoundingClientRect().bottom < window.innerHeight && booksCount.value >= 20) {
-    bookStore.fetchMoreBooks();
-  }
-}
 </script>
