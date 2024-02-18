389ff77a2b8593fe36881ee9674ef8e09881db64
damouu <mouadsehbaoui@gmail.com>
2024-02-18 16:38:15 +0900

    POLARISDPVUEJS-32 Done Unassigned Fixed Done

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row">
+    <section class="mb-4">
+      <TitleMessage title="the given book UUID does not exist."/>
+    </section>
+  </div>
+  <div class="row">
+    <main class="col-12">
+      <TableList :page="pageNumber"/>
+    </main>
+  </div>
+  <div class="row">
+    <section class="mt-4">
+      <PaginationTable @pageNumberEmit="pageEmitReceived"/>
+    </section>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TitleMessage from "@/components/TitleMessage.vue";
+import TableList from "@/components/TableList.vue";
+import PaginationTable from "@/components/PaginationTable.vue";
+import {ref} from "vue";
+
+const pageNumber: Number = ref(0);
+
+function pageEmitReceived(page): void {
+  pageNumber.value = page;
+}
+</script>
