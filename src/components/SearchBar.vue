be7fe497c31005afd3f8e4a0f1bad8a0aac9f469
damouu <mouadsehbaoui@gmail.com>
2024-02-18 16:38:08 +0900

    POLARISDPVUEJS-32 Done Unassigned Fixed Done

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,20 +2,26 @@
   <div class="input-group ps-1">
     <i class="input-group-text bi bi-search "></i>
     <input id="search-input" v-model="input" aria-describedby="basic-addon1" aria-label="Username" class="form-control"
-           placeholder="Search"
-           type="text" @input="toBeCall()">
+           placeholder="Search a Book"
+           type="text" @input="onInput">
   </div>
 </template>
 
 <script lang="ts" setup>
 import {ref} from "vue";
+import debounce from "lodash/debounce"
 
-let input = ref();
+let input = ref<String>("");
 
 const emit = defineEmits<{ (e: 'inputEmit', userInput: String) }>()
 
-function toBeCall() {
-  emit('inputEmit', input.value);
-}
+const onInput = debounce(() => {
+  if (input.value) {
+    emit('inputEmit', input.value);
+  } else if (input.value == "") {
+    emit('inputEmit', null);
+  }
+}, 1000)
+
 
 </script>
