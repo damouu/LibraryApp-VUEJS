066f3a0a63c7e3851443464753ed45408b4cca52
damouu <mouadsehbaoui@gmail.com>
2024-03-13 19:15:43 +0900

    #POLARISDPVUEJS-35  Done Unassigned Fixed Done
    Creates Component: RessourceArt.vue
    
     creates a component to display the cover art of a resource.

---
+++ src/components/RessourceArt.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3>Books art goes here</h3>
+  <br>
+  <img alt="butterfly life cover book"
+       src="https://img.thegoodocs.com/templates/preview/butterfly-life-cover-book-160624.webp"/>
+
+</template>
+
+<script lang="ts" setup>
+
+</script>
