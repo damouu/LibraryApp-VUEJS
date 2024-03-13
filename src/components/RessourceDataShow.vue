146755a817a30bf95cb4830922594174807da824
damouu <mouadsehbaoui@gmail.com>
2024-03-13 19:15:10 +0900

    #POLARISDPVUEJS-35  Done Unassigned Fixed Done
    Creates Component: RessourceDataShow.vue
    
     creates a component to print a resource's data.

---
+++ src/components/RessourceDataShow.vue
@@ -0,0 +1,22 @@
+<template>
+  <h3>Resource data :</h3>
+  <h4>Title : {{ bookStore.book.title }}</h4>
+  <h4>Author : {{ bookStore.book.author }}</h4>
+  <h4>Genre : {{ bookStore.book.genre }}</h4>
+  <h4>Published date :{{ bookStore.book.created_at }}</h4>
+  <h4>Total number of pages :{{ bookStore.book.totalPages }}</h4>
+</template>
+
+<script lang="ts" setup>
+
+import {onMounted} from "vue";
+import {useRoute} from "vue-router";
+import {useBookStore} from "@/stores/Book";
+
+const bookStore = useBookStore();
+
+onMounted(() => {
+  bookStore.getBookUUID(useRoute().params.bookUUID);
+
+})
+</script>
