500baa22879bc4e5b1a852e7d25e6fb835651c7d
damouu <mouadsehbaoui@gmail.com>
2024-03-13 19:19:34 +0900

    #POLARISDPVUEJS-35  Done Unassigned Fixed Done
    Creates: BookView.vue
    
    creates the view to display the components needed to display a resource's data.

---
+++ src/views/BookView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row gy-4">
+    <TitleMessage/>
+    <section class="mb-4 col-4">
+      <ResourceArt/>
+    </section>
+    <section class="col-4 offset-2">
+      <ResourceDataShow/>
+    </section>
+    <section class="col-7 offset-6">
+      <UserDataShow/>
+    </section>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TitleMessage from "@/components/TitleMessage.vue";
+import ResourceArt from "@/components/RessourceArt.vue";
+import ResourceDataShow from "@/components/RessourceDataShow.vue";
+import UserDataShow from "@/components/UserDataShow.vue";
+</script>
