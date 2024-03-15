9e39bc0b17c93152d05ed3e83c71ac38830c8f88
damouu <mouadsehbaoui@gmail.com>
2024-03-15 18:42:03 +0900

    #POLARISDPVUEJS-37 Done Unassigned Fixed Done
    Creates: BorrowBookButton.vue
    
    creates a new component for the borrow button.

---
+++ src/components/BorrowBookButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <router-link v-slot="{ navigate }" custom to="/">
+    <button class="btn rounded ms-2 me-2" role="link" @click="navigate">
+      <i class="bi bi-chat-quote h4">Borrow this book</i>
+    </button>
+  </router-link>
+</template>
+<script lang="ts" setup>
+</script>
