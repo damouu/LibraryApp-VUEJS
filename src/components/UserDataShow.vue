8d5bc05c8feff7c859264015461dd2981648d7e4
damouu <mouadsehbaoui@gmail.com>
2024-03-15 18:48:40 +0900

    #POLARISDPVUEJS-37 Done Unassigned Fixed Done
    Refactor : UserDataShow.vue
    
    refactor the UserDataShow component;
    
     - prevent making two times the same query to the API.
    
    - changes the forwarding page where the user will be guided after clicking the user's studentCardUUID

--- src/components/UserDataShow.vue
+++ src/components/UserDataShow.vue
@@ -1,21 +1,14 @@
 <template>
   <h3>User data :</h3>
-  <RouterLink :to="{ name: 'book', params: { bookUUID: studentStore.student.uuid}}" aria-current="page"
+  <RouterLink :to="{ name: 'UserView', params: { userUUID: bookStore.book.studentIdCard }}" aria-current="page"
               class="nav-link active">
-    <h4>UUID : {{ studentStore.student.uuid }}</h4>
+    <h4>UUID : {{ bookStore.book.studentIdCard }}</h4>
   </RouterLink>
 </template>
 
 <script lang="ts" setup>
-import {onMounted} from "vue";
-import {useStudentStore} from "@/stores/Student";
 import {useBookStore} from "@/stores/Book";
 
-const studentStore = useStudentStore();
 const bookStore = useBookStore();
 
-onMounted(() => {
-  studentStore.getStudentIdCard(bookStore.book.studentIdCard);
-
-})
 </script>
