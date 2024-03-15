07404f6758bcbeba3d1507030554a7147264319e
damouu <mouadsehbaoui@gmail.com>
2024-03-15 15:01:03 +0900

    #POLARISDPVUEJS-36 Done Unassigned Fixed Done
    Refactor: UserDataShow.vue
    
    displays the studentCardId's UUID if the book is currently being borrowed.

--- src/components/UserDataShow.vue
+++ src/components/UserDataShow.vue
@@ -1,7 +1,21 @@
 <template>
   <h3>User data :</h3>
+  <RouterLink :to="{ name: 'book', params: { bookUUID: studentStore.student.uuid}}" aria-current="page"
+              class="nav-link active">
+    <h4>UUID : {{ studentStore.student.uuid }}</h4>
+  </RouterLink>
 </template>
 
 <script lang="ts" setup>
+import {onMounted} from "vue";
+import {useStudentStore} from "@/stores/Student";
+import {useBookStore} from "@/stores/Book";
 
+const studentStore = useStudentStore();
+const bookStore = useBookStore();
+
+onMounted(() => {
+  studentStore.getStudentIdCard(bookStore.book.studentIdCard);
+
+})
 </script>
