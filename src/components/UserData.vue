421a5fa6f84a87c6bc1e843be6af4610e050fc5a
damouu <mouadsehbaoui@gmail.com>
2024-03-15 18:45:26 +0900

    #POLARISDPVUEJS-37 Done Unassigned Fixed Done
    Creates : UserData.vue
    
    creates a new component to print the personal user's information.

---
+++ src/components/UserData.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>User data :</h3>
+  <h4>UUID : {{ studentStore.student.uuid }}</h4>
+  <h4>Name : {{ studentStore.student.name }}</h4>
+  <h4>Email address : {{ studentStore.student.email }}</h4>
+  <h4>Date of Birth :{{ studentStore.student.dob }}</h4>
+</template>
+
+<script lang="ts" setup>
+import {useStudentStore} from "@/stores/Student";
+
+const studentStore = useStudentStore();
+
+</script>
