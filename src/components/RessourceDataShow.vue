c097df6b08c77467f46bd8cea651801b322876f8
damouu <mouadsehbaoui@gmail.com>
2024-03-19 15:35:15 +0900

    #LIBRARYVUE-38 Done Unassigned Fixed Done
    Style: changes component style
    
    changes the component styles

--- src/components/RessourceDataShow.vue
+++ src/components/RessourceDataShow.vue
@@ -1,10 +1,15 @@
-<template>
-  <h3>Resource data :</h3>
+<template xmlns="http://www.w3.org/1999/html">
+  <h3 class="text-primary">About this Book </h3>
+  <br>
   <h4>Title : {{ bookStore.book.title }}</h4>
+  <br>
   <h4>Author : {{ bookStore.book.author }}</h4>
+  <br>
   <h4>Genre : {{ bookStore.book.genre }}</h4>
-  <h4>Published date :{{ bookStore.book.created_at }}</h4>
-  <h4>Total number of pages :{{ bookStore.book.totalPages }}</h4>
+  <br>
+  <h4>Published date : {{ bookStore.book.created_at }}</h4>
+  <br>
+  <h4>Total number of pages : {{ bookStore.book.totalPages }}</h4>
 </template>
 
 <script lang="ts" setup>
